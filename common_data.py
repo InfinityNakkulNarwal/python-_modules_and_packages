fe8ad0a0b61523cf1d38a8b9175b2092470b2a9b
Nakkul Narwal <95772087+InfinityNakkulNarwal@users.noreply.github.com>
2023-01-01 01:25:17 +0530

    Create common_data.py

---
+++ common_data.py
@@ -0,0 +1,10 @@
+#This package contains pre-defined variables of alphabets, numbers, symobols
+
+capital_alphabets = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+lower_case_alphabets = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+numbers = ['1','2', '3','4', '5', '6', '7', '8', '9', '0']
+symbols = ['!','@','#','$','%','^','&','*','(',')','_','+','=','-','*','/','?','>','<',':','"','{','}','[',']','\'',';','.','/','`','~']
+capital_vowals = ['A','I','O','E','U']
+lower_case_vowals = ['a','e','i','o'',u']
+capital_consonants =    ['B','C','D','E','F','G','H','J','K','L','M','N','P','Q','R','S','T','V','W','X','Y','X']
+lower_case_consonants = ['b','c','d','e','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
